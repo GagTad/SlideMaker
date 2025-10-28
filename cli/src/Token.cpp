a4aca394fdba4e89e59e813d79c2feea81857db2
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-28 22:33:54 +0400

    Create Token.cpp

---
+++ cli/src/Token.cpp
@@ -0,0 +1,19 @@
+#include "Token.h"
+
+std::string Token::toString() const{
+
+        std::string typeStr;
+        switch (type) {
+        case TokenType::CREATE_SLIDE: typeStr = "CREATE_SLIDE"; break;
+        case TokenType::LIST_SLIDES:  typeStr = "LIST_SLIDES";  break;
+        case TokenType::SELECT_SLIDE: typeStr = "SELECT_SLIDE"; break;
+        case TokenType::ADD:          typeStr = "ADD";          break;
+        case TokenType::IDENTIFIER:   typeStr = "IDENTIFIER";   break;
+        case TokenType::NUMBER:       typeStr = "NUMBER";       break;
+        case TokenType::STRING_LITERAL: typeStr = "STRING_LITERAL"; break;
+        case TokenType::FLAG:         typeStr = "FLAG";         break;
+        case TokenType::END_OF_INPUT: typeStr = "END_OF_INPUT"; break;
+        case TokenType::UNKNOWN:      typeStr = "UNKNOWN";      break;
+        }
+        return "Token(" + typeStr + ", '" + value + "')";
+}
