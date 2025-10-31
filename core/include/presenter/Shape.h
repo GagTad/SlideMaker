318bf59d02a70b50ec0eb054aef558ea8f238401
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-31 20:14:33 +0400

    Update Shape.h

--- core/include/presenter/Shape.h
+++ core/include/presenter/Shape.h
@@ -4,45 +4,41 @@
 #include <vector>
 #include <map>
 #include <memory> 
+#include <sstream>
 
 class Shape {
 public:
-  
     virtual ~Shape() = default;
-
-     
+    virtual std::string toSvg() const = 0;
     std::map<std::string, std::string> flags;
 };
 
 
- 
 class CircleShape : public Shape {
 public:
-  
-    CircleShape(int cx, int cy, int r)
-        : centerX(cx), centerY(cy), radius(r) {
-    }
+    CircleShape(int cx, int cy, int r);
+    std::string toSvg() const override; 
 
-    
+public:
     int centerX, centerY, radius;
 };
 
  
 class RectangleShape : public Shape {
 public:
-    RectangleShape(int x_coord, int y_coord, int w, int h)
-        : x(x_coord), y(y_coord), width(w), height(h) {
-    }
+    RectangleShape(int x_coord, int y_coord, int w, int h);
+    std::string toSvg() const override;
 
+public:
     int x, y, width, height;
 };
 
  
 class LineShape : public Shape {
 public:
-    LineShape(int start_x, int start_y, int end_x, int end_y)
-        : x1(start_x), y1(start_y), x2(end_x), y2(end_y) {
-    }
+    LineShape(int start_x, int start_y, int end_x, int end_y);
+    std::string toSvg() const override;
 
+public:
     int x1, y1, x2, y2;
 };
