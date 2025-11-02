a980651febe53da45199dbd710fd8d9fbe803441
GagTad <136594120+GagTad@users.noreply.github.com>
2025-11-02 21:11:29 +0400

    Update TokenType.h

--- cli/include/presenter/TokenType.h
+++ cli/include/presenter/TokenType.h
@@ -4,25 +4,25 @@
 
 
 enum class TokenType {
-    
+
+    // Keywords 
     CREATE_SLIDE,  
     LIST_SLIDES,    
     SELECT_SLIDE,   
     ADD,     
     EXPORT,
 
-   
+    // Literals
     IDENTIFIER,     
-
-   
     NUMBER,         
     STRING_LITERAL,
 
-   
+    // Special Symbols
     FLAG,          
 
-   
+    // Meta Tokens
     END_OF_INPUT,   
     UNKNOWN         
 };
+
 
