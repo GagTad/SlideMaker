fdc4fcfac33f77aa4f0e452657519dcbdb951b85
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-19 22:59:44 +0400

    Update Shape.h

--- core/include/presenter/Shape.h
+++ core/include/presenter/Shape.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 #include <vector>
 #include <map>
@@ -10,35 +9,34 @@ class Shape {
 public:
     virtual ~Shape() = default;
     virtual std::string toSvg() const = 0;
+    virtual std::string serialize() const = 0;
     std::map<std::string, std::string> flags;
-};
 
+protected:
+    std::string flagsToString() const;
+    std::string getSvgStyle() const;
+};
 
 class CircleShape : public Shape {
 public:
     CircleShape(int cx, int cy, int r);
-    std::string toSvg() const override; 
-
-public:
+    std::string toSvg() const override;
+    std::string serialize() const override;
     int centerX, centerY, radius;
 };
 
- 
 class RectangleShape : public Shape {
 public:
-    RectangleShape(int x_coord, int y_coord, int w, int h);
+    RectangleShape(int x, int y, int w, int h);
     std::string toSvg() const override;
-
-public:
+    std::string serialize() const override;
     int x, y, width, height;
 };
 
- 
 class LineShape : public Shape {
 public:
-    LineShape(int start_x, int start_y, int end_x, int end_y);
+    LineShape(int x1, int y1, int x2, int y2);
     std::string toSvg() const override;
-
-public:
+    std::string serialize() const override;
     int x1, y1, x2, y2;
 };
