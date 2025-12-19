b46848b74599a178436313241c093a1318004955
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-19 23:00:20 +0400

    Update ShapeFactory.h

--- core/include/presenter/ShapeFactory.h
+++ core/include/presenter/ShapeFactory.h
@@ -1,13 +1,9 @@
 #pragma once
-#pragma once
-
 #include <string>
 #include <vector>
 #include <memory>
-#include <stdexcept>
 #include <map>
 #include "Shape.h" 
-
 
 class IShapeFactory {
 public:
@@ -15,19 +11,13 @@ public:
     virtual std::unique_ptr<Shape> createShape(const std::vector<int>& params) = 0;
 };
 
-
-
 class ShapeRegistry {
 public:
-
     static ShapeRegistry& getInstance() {
         static ShapeRegistry instance;
         return instance;
     }
-
     void registerShape(const std::string& shapeType, std::unique_ptr<IShapeFactory> factory);
-
-
     std::unique_ptr<Shape> createShape(const std::string& shapeType, const std::vector<int>& params);
 
 private:
@@ -35,11 +25,8 @@ private:
     ~ShapeRegistry() = default;
     ShapeRegistry(const ShapeRegistry&) = delete;
     ShapeRegistry& operator=(const ShapeRegistry&) = delete;
-
     std::map<std::string, std::unique_ptr<IShapeFactory>> m_factories;
 };
-
-
 
 class CircleFactory : public IShapeFactory {
 public:
