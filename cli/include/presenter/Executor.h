e775e97a67ef7e219e45a1f64fcecf8f85d6affe
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-21 22:47:53 +0400

    Update Executor.h

--- cli/include/presenter/Executor.h
+++ cli/include/presenter/Executor.h
@@ -10,7 +10,6 @@ public:
     void execute(std::unique_ptr<Command> command);
     void undo();
     void redo();
-
 private:
     Presentation& m_presentation;
     std::vector<std::unique_ptr<Command>> m_undoStack;
