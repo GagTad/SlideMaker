fe643519500a0f1308ac507c259a664f33c6115c
GagTad <136594120+GagTad@users.noreply.github.com>
2026-01-17 21:51:14 +0400

    Add files via upload

---
+++ cli/include/presenter/CommandHistory.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <vector>
+#include <memory>
+#include "Command.h"
+#include "Presentation.h"
+
+class CommandHistory {
+public:
+    void push(std::unique_ptr<Command> command);
+    bool canUndo() const;
+    bool canRedo() const;
+    void undo(Presentation& presentation);
+    void redo(Presentation& presentation);
+
+private:
+    std::vector<std::unique_ptr<Command>> m_undoStack;
+    std::vector<std::unique_ptr<Command>> m_redoStack;
+};
